32d8f057cff6d6a8d6ba34ff6e179fea04782715
tol <tol@Mac-mini-tol.local>
2017-04-13 16:28:30 +0300

    Added fonts Arno pro to page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const container = document.getElementById('root');
+
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  container
 );
