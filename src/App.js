78e197e5a8b422c999d418b14123726ca93a6caa
tol <tol@Mac-mini-tol.local>
2017-04-13 16:37:18 +0300

    added styles for app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import style from './App.css';
-import background from './pictures/background.png'
 import Stroke from './components/common/Stroke/stroke';
 import QuotePage from './components/QuotePage/index';
 
+import style from './App.css';
+
 class App extends Component {
-
-
   render() {
     return (
-      <div style={{backgroundImage: "url('../pictures/background.png')"}}>
+      <div className={style.wrapper}>
         <Stroke />
         <QuotePage />
       </div>
