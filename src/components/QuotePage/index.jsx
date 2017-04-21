69f606dea39b691e57033ca48ed1e5428c1323fb
tol <tol@Mac-mini-tol.local>
2017-04-21 11:28:14 +0300

    added common styles for QuotePage components that aligning elements in it

--- src/components/QuotePage/index.jsx
+++ src/components/QuotePage/index.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import Logo from './Logo/logo';
 import Quote from './Quote/Quote';
 import Dot from './../common/Dot/dot';
+import Author from './../common/Author/author';
+import styles from './index.css';
 
 export default class QuotePage extends React.Component {
-	
+  
 render(){
-	return (
-		<div>
-			<Logo />
-			<Quote />
-			<Dot />
-		</div>
-		);
-	}
+  return (
+    <div className={styles.quoteCommon}>
+      <Logo />
+      <Quote />
+      <Dot />
+      <Author />
+    </div>
+    );
+  }
 }
