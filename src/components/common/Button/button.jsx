4a5d0c49139d8457c11baa1f6081b6068eb58f68
tol <tol@Mac-mini-tol.local>
2017-04-21 12:59:40 +0300

    added button component

---
+++ src/components/common/Button/button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './style.css';
+
+const Button = (props) => {
+		return (
+				<button className={styles.Btn}></button>
+			);
+}
+
+export default Button;
