c0ff08e0ab61261666f6be0e78d0c1663a866ca1
tol <tol@Mac-mini-tol.local>
2017-04-21 12:12:15 +0300

    added Author component

---
+++ src/components/common/Author/author.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './style.css'
+
+const Author = () => {
+	return (
+			<span className={styles.author}>
+				Вольтер
+			</span>
+		)
+}
+
+export default Author;
