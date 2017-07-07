5a4556b7d2e23a53fa7ef83984f8a07168c07ed8
tol <tol@vita.oko.all>
2017-07-07 11:46:23 +0300

    Added opportunity to swipe clic to DescriptionPage and change compoenent on components styled with styled-components module

--- src/components/QuotePage/index.jsx
+++ src/components/QuotePage/index.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import Logo from './Logo/logo';
+import logo from './Logo/head.png';
 import Quote from './Quote/Quote';
+import Center from './../common/Center/index'
 import Dot from './../common/Dot/dot';
 import Author from './../common/Author/author';
 import Button from './../common/Button/button';
-import styles from './index.css';
+
 
 export default class QuotePage extends React.Component {
+
   btnClick(){
     this.props.onChange();
   }
+
 render(){
   return (
-    <div className={styles.quoteCommon}>
-      <Logo />
-      <Quote />
+    <Center>
+      <Logo src={logo} alt='Фраза дня' />
+      <Quote>
+        Картина уничтожения русской интеллигенции 
+        представляется мне территорией, 
+        подвергнутой бомбардировке: некоторые прекрасные здания ещё сохранились, но стоят обнажённо 
+        и одиноко среди руин и разрушенных улиц
+      </Quote>
       <Dot />
-      <Author />
-      <Button goToDescript={this.btnClick.bind(this)}/>
-    </div>
+      <Author> Чак Паланик </Author>
+      <Button onClick={this.btnClick.bind(this)} ></Button>
+    </Center>
     );
   }
 }
+
