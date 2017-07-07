66fa19a213d1b53fcaca70dec32fbec61bbe816d
tol <tol@vita.oko.all>
2017-07-07 11:41:20 +0300

    added style with styled-components to logo component

--- src/components/QuotePage/Logo/logo.jsx
+++ src/components/QuotePage/Logo/logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './head.png';
-import style from './style.css';
+import styled from 'styled-components';
 
-const Logo = () => {
-  return (
-    <img src={logo} className={style.logo} alt="Фраза дня" />
-  )
-}
-
+const Logo = styled.img`
+  width: 326px;
+  height: 132px;
+  position: relative;
+  z-index: 1;
+  margin: 0 auto;
+  display: block;
+`;
 export default Logo;
