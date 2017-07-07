5c4565193cc9675ade498638698231d6acb17e99
tol <tol@vita.oko.all>
2017-07-07 14:32:07 +0300

    added global styles with styled-components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import background from "./pictures/background.png";
+import { fontFace } from "./utils/fontFace";
+import { injectGlobal } from "styled-components";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 
-ReactDOM.render(
-  <App />,
-  container
-);
+ReactDOM.render(<App />, container);
+
+injectGlobal`
+	${fontFace("ArnoPro", "arno-pro")};
+	${fontFace("ArnoProItalic", "arnopro-italic")};
+
+
+	html,
+	body {
+	  margin: 0;
+	  padding: 0;
+	  height: 100%;
+	}
+
+	body {
+	  font: 16px ArnoPro, Georgia, serif;
+	  color: #5b5b5b;
+	  background-image: url(${background});
+	}
+
+	body>div {
+	  height: 100%;
+	}
+`;
