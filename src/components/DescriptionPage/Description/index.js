d64b1dc66bb045d3d76246defe76d98cda6e0744
tol <tol@vita.oko.all>
2017-07-10 17:36:21 +0300

    added Description component to Description Page with styled-component

---
+++ src/components/DescriptionPage/Description/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Description = styled.p`
+	font: 1.7em "ArnoProItalic", Georgia, sans-serif;
+    max-width: 700px;
+    margin: 33px auto 0;
+`;
+
+
+
+export default Description;
