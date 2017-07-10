bc0e8423258d9b1320cad97d28134138723e7ab9
tol <tol@vita.oko.all>
2017-07-10 18:36:42 +0300

    deleted svg fonts from font-face rules

--- src/utils/fontFace.js
+++ src/utils/fontFace.js
@@ -5,10 +5,9 @@ export function fontFace(name, src, fontWeight = 'normal', fontStyle = 'normal')
             font-family: "${name}";
             src: url(${require('../fonts/' + src + '.eot')});
             src: url(${require('../fonts/' + src + '.eot')}?#iefix) format("embedded-opentype"),
+                 url(${require('../fonts/' + src + '.woff2')}) format("woff2"),
                  url(${require('../fonts/' + src + '.woff')}) format("woff"),
-                 url(${require('../fonts/' + src + '.ttf')}) format("truetype"),
-                 url(${require('../fonts/' + src + '.svg')}#${name}) format("svg");
-
+                 url(${require('../fonts/' + src + '.ttf')}) format("truetype");
             font-style: ${fontStyle};
             font-weight: ${fontWeight};
         }
