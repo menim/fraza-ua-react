496c5a5921b67725334551467e38bf96c3870db2
tol <tol@Mac-mini-tol.local>
2017-08-16 17:19:03 +0300

    added padding left and right to description

--- src/components/DescriptionPage/Description/index.js
+++ src/components/DescriptionPage/Description/index.js
@@ -4,6 +4,8 @@ const Description = styled.p`
   font: 1.7em "ArnoProItalic", Georgia, sans-serif;
   max-width: 700px;
   margin: 33px auto 0;
+  padding-left: 15px;
+  padding-right: 15px;
 `;
 
 export default Description;
